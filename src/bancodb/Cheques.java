a3b56ea942c16cb56b7138c67fba3d937a836034
Emmanuel Cázarez <emmanuelcazarez98@gmail.com>
2022-02-21 02:55:36 -0700

    Agregado el control de concurrencia

--- src/bancodb/Cheques.java
+++ src/bancodb/Cheques.java
@@ -12,7 +12,13 @@ public class Cheques extends DBConnection{
 	public void imprimir() {	
 		try {
 			st = con.connection.createStatement();
-			rs = st.executeQuery("select * from "+TCHEQUES);
+			//por defecto se hace autocommit por cada statement sql(para utilizar for update o for share, el autocommit necesita estar desactivado)
+			connection.setAutoCommit(false);
+			//for update bloquea otras transacciones hasta que termine la lectura
+			//for share espera hasta que la transaccion termine para usar los ultimos valores
+			rs = st.executeQuery("select * from "+TCHEQUES+" for share;");
+			//nowait nunca espera a una tupla bloqueada, resultando en un error
+			//skip locked nunca espera a una tupla bloqueada, removiendo las tuplas bloqueadas del result set
 			while (rs.next()) {
 				System.out.println
 				(
@@ -21,23 +27,30 @@ public class Cheques extends DBConnection{
 					rs.getString(TESTTUS).charAt(0)
 				);
 			}
+			connection.commit();
+
 		} catch(SQLException e) {
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
 			e.printStackTrace();
-		}
+		} 
 	}
 	public void depositar(String NOCUENTA, Float IMPORTE) {
 		
 		String query = "update "+TCHEQUES+" set "+TIMPORTE+"="+TIMPORTE+"+ ? where "+TNOCUENTA+"= ? ;";
-		
+	
 		try (PreparedStatement pst = connection.prepareStatement(query)) {
 			
 			pst.setFloat(1, IMPORTE);
 			pst.setString(2, NOCUENTA);
 			pst.executeUpdate();
 			
-		} catch(SQLException e) {
+		} catch(SQLException e) {			
 			e.printStackTrace();
-		}
+		} 
 	}
 	public void retirar(String NOCUENTA, Float IMPORTE) {
 		
--- src/bancodb/main.java
+++ src/bancodb/main.java
@@ -5,7 +5,7 @@ public class main {
 	public static void main(String[] args) {
 		Cheques cheq = new Cheques();
 		
-		cheq.retirar("CHEQ001", Float.valueOf(100));
+		cheq.depositar("CHEQ001", Float.valueOf(100));
 		cheq.imprimir();
 	}
 }
